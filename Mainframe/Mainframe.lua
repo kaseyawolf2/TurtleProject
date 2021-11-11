4776e69ddfd1fc2040d42ce8a8efa03f403dfa8c
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-11 09:51:10 -0600

    Added MF to Require

--- Mainframe/Mainframe.lua
+++ Mainframe/Mainframe.lua
@@ -4,7 +4,8 @@ if not fs.isDir("CraftingKnowledge") then
 end
 
 -- Get the General Functions
-GF = require("GF")
+GF = require("LocalGit/APIs/GF")
+MF = require("MF")
 
 os.setComputerLabel("Mainframe")
 
@@ -34,29 +35,6 @@ rednet.open(peripheral.getName(peripheral.find("modem")))
 
 
 term.setCursorPos(1, 1)
-
-function SearchKnowledge(MFItemName)
-    local ReturnList = {}
-    TTFItemName = string.gsub(MFItemName, ":","-")
-    FoundItems = fs.find("CraftingKnowledge/" .. TTFItemName)
-    if #FoundItems == 0 then
-        return nil
-    end
-    for i = 1, #FoundItems do
-        FResults = fs.open( FoundItems[i] , "r" )
-        x1 = { Itemname = MFItemName }
-        ReturnList[#ReturnList + 1] = { Result = x1 , Ingredients = FResults }
-    end
-    return ReturnList -- return the crafting knowledge
-end
-
-function SaveKnowledge(Knowledge)
-    -- Save Knowledge in the CraftingKnowledge folder
-    KText = string.gsub(Knowledge["Result"]["Itemname"], ":","-")
-    KFile = fs.open("CraftingKnowledge/" .. KText ,"w")
-    KFile.write(textutils.serialize(Knowledge["Ingredients"]))
-    KFile.close()
-end
 
 
 
