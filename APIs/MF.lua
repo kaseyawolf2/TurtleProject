4e2602d51db831a0a5c440829e43cf1b0e0a6ac1
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-15 09:56:41 -0600

    compact MF in to mainframe

--- APIs/MF.lua
+++ APIs/MF.lua
@@ -1,67 +1,66 @@
 local MF = {}
 
-function MF.ListenForStartups()
-    print("Broadcasting Startup")
-    rednet.broadcast("Im Starting Up" , "MainframeOnline")
-    while true do 
-        local Sender, Message, Protocol = rednet.receive("MainframeOnline")
-        print("Start up Detected From : " .. tostring(Sender))
-        if Sender < MyID then
-            MasterMainframeID = Sender
-            return false
-        else
-            rednet.broadcast("I have seniority" , "MainframeOnline")
+function MF.BackupMode()
+    local function MF.ListenForFailure()
+        while true do 
+            local Sender, Message, Protocol = rednet.receive("MainframeFail")
+            print("Start up Detected From : " .. tostring(Sender))
+            if Sender ~= nil then
+                return false
+            end
         end
     end
-end
-
-function MF.ListenForFailure()
-    while true do 
-        local Sender, Message, Protocol = rednet.receive("MainframeFail")
-        print("Start up Detected From : " .. tostring(Sender))
-        if Sender ~= nil then
-            return false
-        end
+    local function MF.MainframeCheckIn()
+        while true do 
+            rednet.broadcast("Hello" , "MainframeRequest")
+            local Sender, Message, Protocol = rednet.receive("MainframeResponce",30)
+            print("Start up Detected From : " .. tostring(Sender))
+            if Sender == nil then
+                return false
+            else
+                sleep(30)
+            end
+        end    
     end
-end
-
-function MF.ListenForMainframe()
-    for i=1,5 do
-        rednet.broadcast("Hello" , "MainframeRequest")
-        print("Mainframe Attempt " .. i .. " : " .. tostring(Sender))
-        local Sender, Message, Protocol = rednet.receive("MainframeResponce",5)
-        if Sender ~= nil then
-            MasterMainframeID = Sender
-            return false
-        end
-    end
-end
-
-function MF.MainframeCheckIn()
-    while true do 
-        rednet.broadcast("Hello" , "MainframeRequest")
-        local Sender, Message, Protocol = rednet.receive("MainframeResponce",30)
-        print("Start up Detected From : " .. tostring(Sender))
-        if Sender == nil then
-            return false
-        else
-            sleep(30)
-        end
-    end    
+    parallel.waitForAny(MF.ListenForFailure,MF.MainframeCheckIn) -- end if Receives Failure message, or Check in fails
 end
 
 function MF.BootMainframe()
+    local function MF.ListenForStartups()
+        print("Broadcasting Startup")
+        rednet.broadcast("Im Starting Up" , "MainframeOnline")
+        while true do 
+            local Sender, Message, Protocol = rednet.receive("MainframeOnline")
+            print("Start up Detected From : " .. tostring(Sender))
+            if Sender < MyID then
+                MasterMainframeID = Sender
+                return false
+            else
+                rednet.broadcast("I have seniority" , "MainframeOnline")
+            end
+        end
+    end
+    local function MF.ListenForMainframe()
+        for i=1,5 do
+            rednet.broadcast("Hello" , "MainframeRequest")
+            print("Mainframe Attempt " .. i .. " : " .. tostring(Sender))
+            local Sender, Message, Protocol = rednet.receive("MainframeResponce",5)
+            if Sender ~= nil then
+                MasterMainframeID = Sender
+                return false
+            end
+        end
+    end
+
     term.clear()
     term.setCursorPos(1, 1)
     print("Mainframe Booting...")
     MasterMainframeID = nil
-
     print("Anyone already the Mainframe or Starting up?")
     parallel.waitForAny(MF.ListenForStartups,MF.ListenForMainframe)
     if MasterMainframeID == nil then
         MasterMainframeID = MyID
     end
-
     term.clear()
 end
 
