b999ad4014ab2ef8241c36d5e0ff2487b5e8c9d1
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-15 09:25:28 -0600

    Requre Gen and Turtle functions

--- Turtle/Starter.lua
+++ Turtle/Starter.lua
@@ -1,7 +1,12 @@
--- await connection to Mainframe 
+GF = require("LocalGit/APIs/GF")
+TF = require("LocalGit/APIs/TF")
+
+
+-- await connection to Mainframe
 -- if no mainframe then ask for one
 -- download GPS Builder
 -- Await resources for it
 -- build it 
 -- delete gps builder
+
 
