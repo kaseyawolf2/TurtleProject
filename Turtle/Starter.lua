e3a927d1c7f1431e17c74dab221060be431e0627
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-15 09:14:44 -0600

    added what to work on

--- Turtle/Starter.lua
+++ Turtle/Starter.lua
@@ -1,4 +1,7 @@
+-- await connection to Mainframe 
+-- if no mainframe then ask for one
+-- download GPS Builder
+-- Await resources for it
+-- build it 
+-- delete gps builder
 
---Connect to MainFrame 
-    --download APIs
-        --
