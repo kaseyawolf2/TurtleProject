8124ce0233897d2ac47579c8b1077acd0f55880a
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-15 09:14:17 -0600

    updated update to update on if its a turtle

--- Installer/Update.lua
+++ Installer/Update.lua
@@ -1,20 +1,36 @@
-if not fs.exists("bbpack") then
-    shell.run("pastebin", "get", "cUYTGbpb","bbpack")
-end
- 
-shell.run("bbpack", "mount", "https://github.com/kaseyawolf2/TurtleProject","Install")
- 
-fs.delete("LocalGit")
-fs.delete("Mainframe.lua")
-fs.delete("Update.lua")
-fs.copy("Install/Mainframe/Mainframe.lua", "Mainframe.lua")
-fs.copy("Install/Installer/Update.lua", "Update.lua")
-fs.copy("Install/","LocalGit")
-
-
--- Make the Mainframe run on Startup
-file = fs.open("Startup.lua","w")
-file.write([[shell.run("Mainframe.lua")]])
-file.close()
- 
+if not turtle then -- check if a turtle or PC
+    -- download and set up
+    if not fs.exists("bbpack") then -- BBpack has a github Downloader
+        shell.run("pastebin", "get", "cUYTGbpb","bbpack")
+    end
+    --Mount Github Repo
+    shell.run("bbpack", "mount", "https://github.com/kaseyawolf2/TurtleProject","Install")
+    --Remove old files and make a local copy from the mounted Repo 
+    fs.delete("LocalGit")
+    fs.delete("Mainframe.lua")
+    fs.delete("Update.lua")
+    fs.copy("Install/Mainframe/Mainframe.lua", "Mainframe.lua")
+    fs.copy("Install/Installer/Update.lua", "Update.lua")
+    fs.copy("Install/","LocalGit")
+    -- Make the Mainframe run on Startup
+    file = fs.open("Startup.lua","w")
+    file.write([[shell.run("Mainframe.lua")]])
+    file.close()
+else-- if a turtle
+    -- download and set up
+    if not fs.exists("bbpack") then -- BBpack has a github Downloader
+        shell.run("pastebin", "get", "cUYTGbpb","bbpack")
+    end
+    --Mount Github Repo
+    shell.run("bbpack", "mount", "https://github.com/kaseyawolf2/TurtleProject","Install")
+    --Remove old files and make a local copy from the mounted Repo 
+    fs.delete("Starter.lua")
+    fs.delete("Update.lua")
+    fs.copy("Install/Turtle/Starter.lua", "Starter.lua")
+    fs.copy("Install/Installer/Update.lua", "Update.lua")
+    -- Make the turtle run on Startup
+    file = fs.open("Startup.lua","w")
+    file.write([[shell.run("Starter.lua")]])
+    file.close()
+end 
 os.reboot()
