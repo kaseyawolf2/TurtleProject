5170534a863fd9f84a7603c8e28b2865c703565f
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-16 17:08:19 -0600

    Check if has Requires passed to it

--- Mainframe/Responder.lua
+++ Mainframe/Responder.lua
@@ -1,16 +1,23 @@
+
+
 GF , MF = ... -- you need to pass them in to multi shell
 
-MasterMainframeID = nil
-MyID = os.getComputerID()
+if GF ~= nil and MF ~= nil then -- are required
 
-while true do
-    MF.BootMainframe()
-    if MasterMainframeID == MyID then
-        MF.ListenRespond() 
-    else
-        term.clear()
-        term.setCursorPos(1, 1)
-        print("Mainframe in Backup Mode")
-        MF.BackupMode()
+    MasterMainframeID = nil
+    MyID = os.getComputerID()
+    
+    while true do
+        MF.BootMainframe()
+        if MasterMainframeID == MyID then
+            MF.ListenRespond() 
+        else
+            term.clear()
+            term.setCursorPos(1, 1)
+            print("Mainframe in Backup Mode")
+            MF.BackupMode()
+        end
     end
+else
+    error("Requires GF and MF to be passed to Program")
 end
