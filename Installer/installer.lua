51a48a74cba6035d1ae4a3b6eb1038b1ab252e78
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-11-16 22:57:08 -0600

    late work psuh

--- Installer/installer.lua
+++ Installer/installer.lua
@@ -1,19 +1,22 @@
 term.clear()
 term.setCursorPos(1, 1)
--- download and set up
-if not fs.exists("/LocalGit/ExternalPrograms/bbpack.lua") then -- BBpack has a github Downloader
+-- download and set up BBpack as it has a github Downloader
+if not fs.exists("/LocalGit/ExternalPrograms/bbpack.lua") then -- if it has a local BBpack lets not download from pastebin
     print("No BBpack Downloading Now")
     shell.run("pastebin", "get", "cUYTGbpb","bbpack")
-    --Mount Github Repo
-    shell.run("bbpack", "mount", "https://github.com/kaseyawolf2/TurtleProject","/Install")
-    fs.delete("bbpack")
 else
     print("BBpack Detected")
-    shell.run("/LocalGit/ExternalPrograms/bbpack.lua", "mount", "https://github.com/kaseyawolf2/TurtleProject","/Install")
+    --Make a Copy of BBpack since were about to delete LocalGit
+    fs.copy("/LocalGit/ExternalPrograms/bbpack.lua","/bbpack")
 end
+--Mount Github Repo
+shell.run("bbpack", "mount", "https://github.com/kaseyawolf2/TurtleProject","/Install")
 --Remove old files and make a local copy from the mounted Repo 
 fs.delete("/LocalGit")
 fs.copy("/Install/","/LocalGit")
+--Delete BBpack so less clutter
+fs.delete("bbpack")
+fs.delete(".bbpack.cfg")
 -- Make the Mainframe run on Startup
 fs.delete("/Startup.lua")
 file = fs.open("/Startup.lua","w")
@@ -34,4 +37,5 @@ shell.run("Starter")
 ]])
     file.close()
 end 
+
 os.reboot()
