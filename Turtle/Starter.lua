1ec880416ab3b2cc3529cccc574bff7c041483ed
Kasey Keahey <kasey_keahey@yahoo.com>
2021-11-24 21:35:42 -0600

    Starter Start

--- Turtle/Starter.lua
+++ Turtle/Starter.lua
@@ -1,13 +1,31 @@
 GF = require("/LocalGit/APIs/GF")
 TF = require("/LocalGit/APIs/TF")
 
+while gpsConnection == false do
+    if gps.locate(5) ~= nil then
+        gpsConnection = true
+    else
+        --No GPS Connection Detected
+        print("No GPS Detected")
+        shell.run("LocalGit/ExternalPrograms/gps-deploy")
+        
+    end
+    
+end
 
--- await connection to Mainframe
--- if no mainframe then ask for one
--- download GPS Builder
--- Await resources for it
--- build it 
--- delete gps builder
+--Connect to GPS System
+    --If no GPS Download GPS Builder and build One
 
-local x  = GF.MainframeConnect()
-print(x)
+--Connect to Mainframe
+    --if no mainframe then ask for one and wait
+
+--Ask for Job From Mainframe
+    --Go into Long Term Storage
+        --Go to Storage Area and get put in to chest
+    --Go into Short Term Storage
+        --Holding Pen (1x1 Chunk)
+        --Hold Postion
+    --Become a Miner
+    --Become a Crafter
+    --Become a Builder
+    --Become a Storage Bot
