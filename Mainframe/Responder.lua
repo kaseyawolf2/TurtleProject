476f04c60088804ced9df746b0898d44011c5fa8
Kasey Keahey <Kasey_Keahey@yahoo.com>
2022-03-31 18:46:10 -0700

    Skip Bootup

--- Mainframe/Responder.lua
+++ Mainframe/Responder.lua
@@ -19,18 +19,21 @@ if GF ~= nil and MF ~= nil then -- APIs are required
 
     MasterMainframeID = nil
     MyID = os.getComputerID()
-    
     while true do
-        MF.BootMainframe()
-        if MasterMainframeID == MyID then
-            MF.ListenRespond() 
-        else
-            term.clear()
-            term.setCursorPos(1, 1)
-            print("Mainframe in Backup Mode")
-            MF.BackupMode()
-        end
+        MF.ListenRespond()
     end
+    read()
+    -- while true do
+    --     MF.BootMainframe()
+    --     if MasterMainframeID == MyID then
+    --         MF.ListenRespond() 
+    --     else
+    --         term.clear()
+    --         term.setCursorPos(1, 1)
+    --         print("Mainframe in Backup Mode")
+    --         MF.BackupMode()
+    --     end
+    -- end
 else
     error("Requires GF and MF to be passed to Program")
 end
