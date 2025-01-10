c2727a162ca359a387500c03c06a85e41b63b877
Kaseyawolf2 <Kasey_Keahey@yahoo.com>
2025-01-09 17:32:16 -0800

    Refactor Responder.lua to streamline API requirements and enhance mainframe boot process

--- Mainframe/Responder.lua
+++ Mainframe/Responder.lua
@@ -1,27 +1,144 @@
 if term.isColor() then -- if advanced
     if not multishell then -- and doesnt have access to Multishell 
         --we are in a multishell so Require dont work
-        GF , MF = ... -- you need to pass them in to multi shell
+        GF = ... -- you need to pass them in to multi shell
     else
         --not in a muiltishell so require the APIs 
-        GF = require("/LocalGit/APIs/GF")
-        MF = require("/LocalGit/APIs/MF")
+        GF = require("APIs/GF")
     end
 else
     --not in a advanced pc so just require the APIs 
-    GF = require("/LocalGit/APIs/GF")
-    MF = require("/LocalGit/APIs/MF")
+    GF = require("APIs/GF")
 end
 
+if GF ~= nil then -- APIs are required
+    AF = require("APIs/AF")
+    MessageQueue = {}
 
+    local function BackupMode()
+        local function ListenForFailure()
+            while true do 
+                local Sender, Message, Protocol = rednet.receive("MainframeFail")
+                print("Start up Detected From : " .. tostring(Sender))
+                if Sender ~= nil then
+                    return false
+                end
+            end
+        end
+        local function MainframeCheckIn()
+            while true do 
+                rednet.broadcast("Hello" , "MainframeRequest")
+                local Sender, Message, Protocol = rednet.receive("MainframeResponce",30)
+                print("Start up Detected From : " .. tostring(Sender))
+                if Sender == nil then
+                    return false
+                else
+                    sleep(30)
+                end
+            end    
+        end
+        parallel.waitForAny(ListenForFailure,MainframeCheckIn)
+    end
 
-if GF ~= nil and MF ~= nil then -- APIs are required
+    local function BootMainframe()
+        local function ListenForStartups()
+            print("Broadcasting Startup")
+            rednet.broadcast("Im Starting Up" , "MainframeOnline")
+            while true do 
+                local Sender, Message, Protocol = rednet.receive("MainframeOnline")
+                print("Start up Detected From : " .. tostring(Sender))
+                if Sender < os.getComputerID() then
+                    MasterMainframeID = Sender
+                    return false
+                else
+                    rednet.broadcast("I have seniority" , "MainframeOnline")
+                end
+            end
+        end
+        local function ListenForMainframe()
+            for i=1,5 do
+                rednet.broadcast("Hello" , "MainframeRequest")
+                print("Mainframe Attempt " .. i .. " : " .. tostring(Sender))
+                local Sender, Message, Protocol = rednet.receive("MainframeResponce",5)
+                if Sender ~= nil then
+                    MasterMainframeID = Sender
+                    return false
+                end
+            end
+        end
+        --Open Rednet
+        rednet.close()
+        sleep(1)
+        rednet.open(peripheral.getName(peripheral.find("modem")))
+        --Clear Screen and reset to top
+        term.clear()
+        term.setCursorPos(1, 1)
+        print("Mainframe Booting...")
+        MasterMainframeID = nil
+        print("Anyone already the Mainframe or Starting up?")
+        parallel.waitForAny(ListenForStartups,ListenForMainframe)
+        if MasterMainframeID == nil then
+            MasterMainframeID = os.getComputerID()
+        end
+        term.clear()
+    end
 
+    local function Listen()
+        --Listen for Message
+        local Event, Sender, Message, Protocol = os.pullEvent("rednet_message")
+        --Package Message
+        local MessagePackage = {Event = Event,Sender = Sender,Message = Message, Protocol = Protocol}
+        --Add package to table
+        table.insert(MessageQueue,MessagePackage)
+    end
+
+    local function Respond()
+        if #MessageQueue > 0 then
+            local MessagePackage = table.remove(MessageQueue,1)
+            local Event =   MessagePackage["Event"]
+            local Sender =  MessagePackage["Sender"]
+            local Message = MessagePackage["Message"]
+            local Protocol =MessagePackage["Protocol"]
+
+            if Protocol == "MineSlotRequest" then
+                local MD = require("APIs/MD")
+                local Area = AF.LoadArea(Message)
+                CurAssigned = Area["SlicesAssigned"]
+                AssignNum = 1 + CurAssigned
+                Area["SlicesAssigned"] = AssignNum
+                print("Assigning " .. AssignNum ..  " To " .. Sender)
+                rednet.send(Sender, AssignNum ,"MineSlotAssignment")
+                AF.SaveArea(Area["ID"],Area)
+
+            elseif Protocol == "MineRequest" then
+                local MD = require("APIs/MD")
+                local Area = AF.LoadArea(1)
+                CurAssigned = Area["SlicesAssigned"]
+                AssignNum = 1 + CurAssigned
+                Area["SlicesAssigned"] = AssignNum
+                print("Assigning " .. AssignNum ..  " To " .. Sender)
+                Pack = {Area = AF.LoadArea(Area["ID"]), Slice = AssignNum}
+                AF.SaveArea(Area["ID"],Area)
+                rednet.send(Sender, Pack ,"MineSlotAssignment")
+            else
+                print("Unknown Message from " .. Sender .. " with Protocol " .. tostring(Protocol))
+            end
+        else 
+            print("Message Queue now : " .. #MessageQueue)
+        end
+    end
+
+    -- Main execution
     MasterMainframeID = nil
     MyID = os.getComputerID()
+    
+    -- Boot up the mainframe
+    BootMainframe()
+    
+    -- Main loop
     while true do
-        parallel.waitForAll(MF.Listen, MF.Respond) 
+        parallel.waitForAll(Listen, Respond)
     end
 else
-    error("Requires GF and MF to be passed to Program")
+    error("Requires GF to be passed to Program")
 end
